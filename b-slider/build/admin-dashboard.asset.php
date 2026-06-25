a1013b940f8cf95fd611af1727f296dd9bab3a40
Sean Murphy <sean@iamseanmurphy.com>
2026-06-25 11:10:05 +0000

    2.0.14

--- b-slider/build/admin-dashboard.asset.php
+++ b-slider/build/admin-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => '446f8954a530c28c9143');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '2de18bc9b9578001606a');
