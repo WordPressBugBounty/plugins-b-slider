e51fd649d0488a0ab4409d9f0162ff7e9cafdb38
Sean Murphy <sean@iamseanmurphy.com>
2026-08-18 13:07:29 +0000

    2.0.19

--- b-slider/build/index.asset.php
+++ b-slider/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6bb2bcfe0583fdaf5084');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '82a81c9350ab3bcda6e2');
--- b-slider/build/view.asset.php
+++ b-slider/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '518277f31dd5ee3783d3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '229706ad007201bdbcfc');
