116cf351c17b1fca4f344ff5363caba94d6ade10
Sean Murphy <sean@iamseanmurphy.com>
2026-08-19 07:03:16 +0000

    2.0.20

--- b-slider/build/index.asset.php
+++ b-slider/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '82a81c9350ab3bcda6e2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '572f7d31c00cb374e2e2');
